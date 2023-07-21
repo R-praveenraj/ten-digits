1e2a074e537431e23e9af58ed0bf30088c4dc417
praveenraj <112162475+R-praveenraj@users.noreply.github.com>
2023-07-21 13:10:48 +0530

    Update tenDigitSorting.py

--- tenDigitSorting.py
+++ tenDigitSorting.py
@@ -1,10 +1,9 @@
-def tenDigit(number):
+def tensDigit(number):
     number //= 10
     return number % 10
-
 def tens_digit_sort(array):
-    array.sort(key=lambda x: (tens_digit(x), -x))
+    array.sort(key=lambda x: (tensDigit(x), -x))
     return array
-
+    
 array=list(map(int,input().split()))
-print(tenDigit(array))
+print(tens_digit_sort(array))
